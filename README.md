de958f1f8769d617681449b0ad52db7d460dea3a
CodingOnMars <15357252+CodingOnMars@users.noreply.github.com>
2021-09-15 15:44:50 +0300

    Added cart toggle functionality. Added note to readme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Comfy store
 
 > E-commerce project built with HTML5, CSS3 and ES6+ during John Smilga [JavaScript course](https://www.udemy.com/course/javascript-tutorial-for-beginners-w/).
+
+> NOTE: I used John's course as a reference, some things I did in a different way.
--- src/cart/toggleCart.js
+++ src/cart/toggleCart.js
@@ -1,3 +1,17 @@
 import { getElement } from '../utils.js';
 
-export const openCart = () => {};
+const cartOverlay = getElement('.cart-overlay');
+const cartIcon = getElement('.toggle-cart');
+const cartClose = getElement('.cart-close');
+
+cartIcon.addEventListener('click', () => {
+  cartOverlay.classList.add('show');
+});
+
+cartClose.addEventListener('click', () => {
+  cartOverlay.classList.remove('show');
+});
+
+export const openCart = () => {
+  cartOverlay.classList.add('show');
+};
