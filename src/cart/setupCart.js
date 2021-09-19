22be8cea8d7db7c22e4f857f2da14993d8b9906a
CodingOnMars <15357252+CodingOnMars@users.noreply.github.com>
2021-09-19 10:49:30 +0300

    initial setup

--- src/cart/setupCart.js
+++ src/cart/setupCart.js
@@ -8,9 +8,22 @@ import {
 import { openCart } from './toggleCart.js';
 import { findProduct } from '../store.js';
 import addToCartDOM from './addToCartDOM.js';
+
 // set items
+const cratItemCountDOM = getElement('.cart-item-count');
+const cartItemsDOM = getElement('.cart-items');
+const cartTotalDOM = getElement('.cart-total');
+
+// getStorageItem returns an empty array if there was no data provided
+let cart = getStorageItem('cart');
 
 export const addToCart = (id) => {
   console.log(id);
   openCart();
 };
+
+const init = () => {
+  // console.log(cart); // if there is no data, returns an empty array
+};
+
+init();
